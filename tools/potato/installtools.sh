986415a4df95de8777cd73659abad22613c12041
Matt Porter <porter@debian.org>
1999-11-19 00:17:36 +0000

    * Fix support for installing powerpc documentation.  It is in a different   directory name in the current 2.2.1 boot-floppies release.  This will   change.

--- tools/potato/installtools.sh
+++ tools/potato/installtools.sh
@@ -6,16 +6,22 @@ set -e
 
 BDIR=$TDIR/$CODENAME-$ARCH
 
+if [ "$ARCH" = "powerpc" ]; then
+	DOCDIR="docs"
+else
+	DOCDIR="documentation"
+fi
+
 # Put the install documentation in /install
-cd $BDIR/1/dists/$CODENAME/main/disks-$ARCH/current/documentation
-mkdir $BDIR/1/install/documentation
-cp *.{html,txt} $BDIR/1/install/documentation/
+cd $BDIR/1/dists/$CODENAME/main/disks-$ARCH/current/$DOCDIR
+mkdir $BDIR/1/install/$DOCDIR
+cp *.{html,txt} $BDIR/1/install/$DOCDIR/
 ln -sf install*.html $BDIR/1/install/index.html
 
 # Put the boot-disk documentation in /doc too
 mkdir $BDIR/1/doc/install
 cd $BDIR/1/doc/install
-for file in ../../install/documentation/*.{html,txt}
+for file in ../../install/$DOCDIR/*.{html,txt}
 do
 	ln -s $file
 done
