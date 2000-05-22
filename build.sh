faf851e3dded4141f965ca1d82ac3462e38b6b3c
Ben Collins <bcollins@debian.org>
2000-05-22 14:47:28 +0000

    oops

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ make mirrorcheck
 disks=`du -sm ${MIRROR}/dists/${CODENAME}/main/disks-${ARCH}/current/. | \
 	awk '{print $1}'`
 make list COMPLETE=1 SIZELIMIT1=$(((630 - ${disks}) * 1024 * 1024)) SRCSIZELIMIT=$((635 * 1024 * 1024))
-make bin-official_images
+make official_images
