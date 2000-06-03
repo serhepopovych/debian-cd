d542709dc5b6f8d3c3de004df13de0396602eade
Raphaël Hertzog <hertzog@debian.org>
2000-06-03 13:16:06 +0000

    * Include the upgrade directories when available.

--- tools/potato/upgrade.sh
+++ tools/potato/upgrade.sh
@@ -3,4 +3,16 @@
 # FOR POTATO
 # Include upgrade* dir when available
 
+set -e
+
+for arch in i386 alpha sparc m68k
+do
+  if [ "$ARCH" = "$arch" -a -d "$MIRROR/dists/$CODENAME/main/upgrade-$ARCH" ];
+  then
+    cp -a $MIRROR/dists/$CODENAME/main/upgrade-$ARCH $TDIR/$CODENAME-$ARCH/1/
+    mv $TDIR/$CODENAME-$ARCH/1/upgrade-$ARCH $TDIR/$CODENAME-$ARCH/1/upgrade
+  fi
+done
+
 exit 0
+
