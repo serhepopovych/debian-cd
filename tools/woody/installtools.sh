be220942c40286631ae8194c9cc7f3558509b072
Jim Westveer <jwest@debian.org>
2000-07-06 00:40:50 +0000

    update to /tools/boot/woody/i386 to check for existance of bootdisks --jwest

--- tools/woody/installtools.sh
+++ tools/woody/installtools.sh
@@ -2,28 +2,35 @@
 
 # Install files in /install and some in /doc
 # 26-dec-99 changes for i386 (2.2.x) bootdisks --jwest
-
+# 11-mar-00 added sparc to boot-disk documentation test  --jwest
+# 30-jun-00 synced with potato updates --jwest
+# 05-JUL-00 added CODENAME1 and test for existance of woody bootdisks --jwest
 set -e
 
-BDIR=$TDIR/$CODENAME-$ARCH
+# The location of the tree for CD#1, passed in
+DIR=$1
 
-if [ "$ARCH" = "powerpc" ]; then
-	DOCDIR="docs"
- elif [ "$ARCH" = "i386" ] ; then
-         DOCDIR="doc"
+DOCDIR=doc
+
+if [ -f $MIRROR/dists/$CODENAME/main/disks-$ARCH/current/images-2.88/compact/res
+cue.bin ]; then
+        echo "Using woody bootdisks"
+        CODENAME1=$CODENAME
 else
-	DOCDIR="documentation"
+        echo "Using potato bootdisks"
+        CODENAME1=potato
 fi
 
+
 # Put the install documentation in /install
-cd $BDIR/1/dists/$CODENAME/main/disks-$ARCH/current/$DOCDIR
-mkdir $BDIR/1/install/$DOCDIR
-cp *.{html,txt} $BDIR/1/install/$DOCDIR/
-ln -sf install*.html $BDIR/1/install/$DOCDIR/index.html
+cd $DIR/dists/$CODENAME1/main/disks-$ARCH/current/$DOCDIR
+mkdir $DIR/install/$DOCDIR
+cp -a * $DIR/install/$DOCDIR/
+ln -sf install.en.html $DIR/install/$DOCDIR/index.html
 
 # Put the boot-disk documentation in /doc too
-mkdir $BDIR/1/doc/install
-cd $BDIR/1/doc/install
+mkdir $DIR/doc/install
+cd $DIR/doc/install
 for file in ../../install/$DOCDIR/*.{html,txt}
 do
 	ln -s $file
