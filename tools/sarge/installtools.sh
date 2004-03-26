00930ef2e164cfe5034866e67d1fefd32febd769
Joey Hess <joeyh@debian.org>
2004-03-26 18:07:27 +0000

    - Include sarge installation manual on sarge CDs.     - Remove install/doc, and just use doc/install.

--- tools/sarge/installtools.sh
+++ tools/sarge/installtools.sh
@@ -15,28 +15,13 @@ else
         DOCS=$MIRROR/dists/sid/main/installer-$ARCH/current/$DOCDIR
 fi
 
-# Put the install documentation in /install
+# Put the install documentation in /doc/install
 if [ ! -d $DOCS ]; then
     echo "ERROR: Unable to copy installer documentation to CD."
     exit
 fi
 cd $DOCS
-mkdir -p $DIR/install/$DOCDIR
-if cp -a * $DIR/install/$DOCDIR/ ; then
-    ln -f $DIR/install/$DOCDIR/index.en.html $DIR/install/$DOCDIR/index.html
-else
+mkdir -p $DIR/$DOCDIR/install
+if ! cp -a * $DIR/$DOCDIR/install; then
     echo "ERROR: Unable to copy installer documentation to CD."
 fi
-
-# Put the installer documentation in /doc too
-mkdir -p $DIR/doc/install
-cd $DIR/doc/install
-for file in ../../install/$DOCDIR/*.html
-do
-    ln $file
-done
-if [ -e ../../install/$DOCDIR/INSTALLATION-HOWTO ]; then
-    ln -f ../../install/$DOCDIR/INSTALLATION-HOWTO
-fi
-
-# FIXME: why does it have to be in two places?
