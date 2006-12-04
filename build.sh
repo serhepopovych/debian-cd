0d08a2a2d7b871d1ccfb1069a1c86c7627448400
Steve McIntyre <93sam@debian.org>
2006-12-04 11:41:28 +0000

    Fix simple logic problem: s///

--- build.sh
+++ build.sh
@@ -68,16 +68,15 @@ if [ -n "$NOSOURCE" ] ; then
 fi
 make $LISTTARGET $SIZE_ARGS SRCSIZELIMIT=$FULL_SIZE
 
-echo " ... building the images"
 # Setting IMAGETARGET directly is deprecated; NOSOURCE is preferred
 if [ -z "$IMAGETARGET" ]; then
 	if [ -n "$NOSOURCE" ] ; then
 	    IMAGETARGET="bin-official_images"
-	fi
-	if [ -z "$IMAGETARGET" ] ; then
+	else
 	    IMAGETARGET="official_images"
 	fi
 fi
+echo " ... building the images; using target(s) \"$IMAGETARGET\""
 make $IMAGETARGET
 
 make imagesums
