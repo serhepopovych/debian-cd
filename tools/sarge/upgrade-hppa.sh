07a91b45e337db9a3de3b2eec67624cef1701abb
Frans Pop <fjp@debian.org>
2008-10-14 08:02:21 +0000

    Drop support for building Sarge CDs

--- tools/sarge/upgrade-hppa.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "Copying the upgrade bits for hppa"
-cp -a $MIRROR/dists/sarge/main/upgrade-kernel $1/CD1/upgrade-kernel
-
-exit 0
--- tools/sarge/upgrade-sparc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "Copying the upgrade bits for sparc"
-cp -a $MIRROR/dists/sarge/main/upgrade-kernel $1/CD1/upgrade-kernel
-
-exit 0
