8f45f720339e2d0c44ec486af46b46c42336508d
cd-builder user <debian-cd@lists.debian.org>
2010-07-02 23:24:56 +0000

    current script versions from farbror

--- contrib/settings.sh
+++ contrib/settings.sh
@@ -1,15 +1,15 @@
-export TRACE=/org/cdbuilder.debian.org/src/ftp/debian/project/trace/farbror.acc.umu.se
+export TRACE=/org/cdbuilder.debian.org/src/ftp/debian/project/trace/pettersson.debian.org
 export ARCH_DI_DIR=/org/cdbuilder.debian.org/src/deb-cd/d-i
 export HOSTNAME=`hostname -f`
 
 export PUBDIR=/org/cdbuilder.debian.org/dst/deb-cd
 
 export MIRROR=/org/cdbuilder.debian.org/src/ftp/debian
-export BASEDIR=/home/deb-cd/build/debian-cd
-export MKISOFS=/home/deb-cd/build/mkisofs/usr/bin/mkisofs
+export BASEDIR=~/build/debian-cd
+export MKISOFS=~/build/mkisofs/usr/bin/mkisofs
 if [ "$DATE"x = ""x ] ; then
     export DATE=`date -u +%Y%m%d`
 fi
-export PARALLEL=0
+export PARALLEL=1
 
-export EXTRACTED_SOURCES=/export/ftp/cdimage/cd-sources
+export EXTRACTED_SOURCES=/mnt/nfs-cdimage/cd-sources
