3e2382d4ad18da9d531e2a71c393f4f89471a55a
Didier Raboud <odyx@debian.org>
2015-08-21 15:10:59 +0200

    Fix booting from help files and isolinux.cfg overwriting

--- tools/boot/stretch/x86-desktop.sh
+++ tools/boot/stretch/x86-desktop.sh
@@ -10,30 +10,6 @@ di_syslinux_version() {
 	echo "$version"
 }
 
-# Workaround for #505243
-# Syslinux does not correctly handle a default64 option in combination
-# with vesamenu. Instead, add special default label to automatically
-# select i386/amd64 if user hits enter from help screens.
-multiarch_workaround() {
-	if [ -f $CDDIR/../syslinux/usr/lib/syslinux/modules/bios/ifcpu64.c32 ] ; then
-		cp -f $CDDIR/../syslinux/usr/lib/syslinux/modules/bios/ifcpu64.c32 boot$N/isolinux/
-	else
-		cp -f $CDDIR/../syslinux/usr/lib/syslinux/ifcpu64.c32 boot$N/isolinux/
-	fi
-	sed -i "/^default install/ s/^/#/" \
-		boot$N/isolinux/txt.cfg || true
-	sed -i "/^default64 amd64-install/ s/^/#/" \
-		boot$N/isolinux/amdtxt.cfg || true
-	sed -i "/^include menu.cfg/ a\include instsel.cfg" \
-		boot$N/isolinux/prompt.cfg
-	cat >boot$N/isolinux/instsel.cfg <<EOF
-default install-select
-label install-select
-    kernel ifcpu64.c32
-    append amd64-install -- install
-EOF
-}
-
 set_default_desktop() {
 	# Set default desktop, or remove if not applicable
 	if [ "$DESKTOP" ]; then
