7dcb2cff5b5b34d1eb99e272ae20b21dd5e4b5b9
Cyril Brulebois <kibi@debian.org>
2015-08-27 01:36:08 +0200

    Make stretch the default suite in the easy-build.sh script.

--- easy-build.sh
+++ easy-build.sh
@@ -73,10 +73,10 @@ fi
 ## For what release to build images
 
 # The suite the installed system will be based on
-export CODENAME=wheezy
+export CODENAME=stretch
 # The suite from which the udebs for the installer will be taken (normally
 # the same as CODENAME)
-export DI_CODENAME=wheezy
+export DI_CODENAME=stretch
 
 
 ## The debian-installer images to use. This must match the suite (DI_CODENAME)
